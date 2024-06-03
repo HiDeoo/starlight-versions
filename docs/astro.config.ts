ad3a8ef9f97c2ffbbfc6c3e2accc297de3c95bc2
HiDeoo <494699+HiDeoo@users.noreply.github.com>
2024-06-03 11:32:58 +0200

    docs: add documentation index page

--- docs/astro.config.ts
+++ docs/astro.config.ts
@@ -1,39 +1,22 @@
 import starlight from '@astrojs/starlight'
 import { defineConfig } from 'astro/config'
-import starlightVersions from 'starlight-versions'
 
 export default defineConfig({
   integrations: [
     starlight({
-      // TODO(HiDeoo)
-      plugins: [
-        starlightVersions({
-          versions: [
-            {
-              slug: '5.1',
-            },
-            {
-              slug: '5.0',
-            },
-          ],
-        }),
-      ],
-      // TODO(HiDeoo)
       customCss: ['./src/styles/custom.css'],
       editLink: {
         baseUrl: 'https://github.com/HiDeoo/starlight-versions/edit/main/docs/',
       },
-      // TODO(HiDeoo)
       sidebar: [
         {
           label: 'Start Here',
           items: [{ label: 'Getting Started', link: '/getting-started/' }],
         },
         {
-          label: 'Components',
-          autogenerate: { directory: 'components' },
+          label: 'Guides',
+          autogenerate: { directory: 'guides' },
         },
-        { label: 'Demo', link: '/demo/' },
       ],
       social: {
         github: 'https://github.com/HiDeoo/starlight-versions',
--- docs/src/content/config.ts
+++ docs/src/content/config.ts
@@ -1,8 +1,6 @@
 import { docsSchema } from '@astrojs/starlight/schema'
 import { defineCollection } from 'astro:content'
-import { docsVersionsSchema } from 'starlight-versions/schema'
 
 export const collections = {
   docs: defineCollection({ schema: docsSchema() }),
-  versions: defineCollection({ type: 'data', schema: docsVersionsSchema() }),
 }
