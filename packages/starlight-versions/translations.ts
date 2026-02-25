29c9f418b59c4ccc4f5c2bb41d3acfa08b7b26ea
Felix Schneider <99918022+trueberryless@users.noreply.github.com>
2026-02-25 15:48:40 +0100

    feat(i18n): add German translation

--- packages/starlight-versions/translations.ts
+++ packages/starlight-versions/translations.ts
@@ -8,4 +8,13 @@ export const Translations = {
     'starlightVersions.search.outdated.slug': 'Search limited to the {{slug}} version.',
     'starlightVersions.select.accessibleLabel': 'Select version',
   },
+  de: {
+    'starlightVersions.link.latest': 'Geh zur <a href={{link}}>neuesten Version</a>, um die aktuellste Dokumentation zu bekommen.',
+    'starlightVersions.outdated.label': 'Dieser Inhalt ist für {{label}}.',
+    'starlightVersions.outdated.slug': 'Dieser Inhalt ist für die Version {{slug}}.',
+    'starlightVersions.search.link.latest': 'Geh zur <a href={{link}}>neuesten Version</a>, um aktuelle Ergebnisse zu bekommen.',
+    'starlightVersions.search.outdated.label': 'Suche beschränkt auf {{label}}.',
+    'starlightVersions.search.outdated.slug': 'Suche nur in der Version {{slug}}.',
+    'starlightVersions.select.accessibleLabel': 'Version auswählen',
+  },
 }
